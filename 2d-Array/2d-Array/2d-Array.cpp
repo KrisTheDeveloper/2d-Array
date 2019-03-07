bad5af957801876736db02432a7fd6b8e8eeb220
Kris <kswhite609@hotmail.com>
2019-03-07 01:39:48 -0500

    Current iteration demostrates correct results. I could do a single hour glass in 3 column loops. no need to jump between Rows.

--- 2d-Array/2d-Array/2d-Array.cpp
+++ 2d-Array/2d-Array/2d-Array.cpp
@@ -9,124 +9,61 @@
 using namespace std;
 
 int hourglassSum(vector<vector<int>> arr) 
-{
-	
-	
+{	
 	int currentSum = 0;
-	int maxSum = 0;
+	int maxSum = -1000;
 
 	int homeRow = 0;
 	int homeCol = 0;
 	
-	//Won't work this way. Will need to change both forloops to standard int::iterators with arr.size(row) or arr.size[0](column) being the max.
-	//will then need to keep track of 0 location of both row and column and capture 1 hour glass at a time only overwriting sum if th current hourglass is greater than the previous one
-	//there will be 16 hourglass checks but numbers will be iterated more than once. Around twice for most of the numbers but this will be the best way without an algorithm. 
-	//this process is better. I need to mess with it though to rid defects.
 	for (int rowIter = homeRow; rowIter != arr.size(); )
 	{
-		
-		for (int colIter = homeCol; colIter != arr[0].size(); colIter++)
+		for (int colIter = homeCol; colIter != (arr[0].size() + 1); colIter++)
 		{
-			if (!(homeCol < (arr[0].size() - 2)))
+			if (homeCol > 3)
 			{
 				homeRow++;
 				rowIter = homeRow;
 				homeCol = 0;
 				break;
-			}
-			else if (!(rowIter < (homeRow + 3)))
-			{
-				homeCol++;
-				rowIter = homeRow;
-				break;
-			}
+			}			
 			else if (rowIter == (homeRow + 1))
 			{
 				if (colIter == (homeCol + 1))
 				{
-					cout << arr[rowIter][colIter] << endl;
 					currentSum += arr[rowIter][colIter];
-					homeRow++;
-					rowIter = homeRow;
+					rowIter++;
 					break;
 				}
 				continue;				
 			}
 			else if (colIter < (homeCol + 3) && rowIter != (homeRow +1))
 			{
-				cout << arr[rowIter][colIter] << endl;
 				currentSum += arr[rowIter][colIter];
 				continue;
 			}
-
-			//if (rowIter != (homeRow + 1))
-			//{				
-				rowIter++;
-			//}
-
 			
-		}
-		
-	}
-	cout << "first sum = " << currentSum;
-	/*
-	for (vector<vector<int>>::iterator rowIter = arr.begin(); rowIter != arr.end(); rowIter++)
-	{		
-		/*if (rowIndex % 2 > 0)
-		{
-			cout << "Odd row";
-			flagSkipRow = true;
-		}
-		else
-			flagSkipRow = false;
-		
-		int columnIndex = 0;
-		for(vector<int>::iterator colIter = rowIter->begin(); colIter != rowIter->end(); colIter++)
-		{	
-			//3on - 4off - 1on - 4off - 3on
-			int currentNum = *colIter;	
-
-			
-
-			//going to simplify this logic
-			switch (columnIndex)
+			if (!(rowIter < (homeRow + 2)))
 			{
-				case 0:
-					sum[0] += currentNum;
-					break;
-				case 1:
-					sum[0] += currentNum;
-					sum[1] += currentNum;
-					break;
-				case 2:
-					sum[0] += currentNum;
-					sum[1] += currentNum;
-					sum[2] += currentNum;
-					break;
-				case 3:
-					sum[1] += currentNum;
-					sum[2] += currentNum;
-					sum[3] += currentNum;
-					break;
-				case 4:
-					sum[2] += currentNum;
-					sum[3] += currentNum;
-					break;
-				case 5:
-					sum[3] += currentNum;
+				homeCol++;
+				rowIter = homeRow;
+				if (currentSum > maxSum)
+					maxSum = currentSum;
+				
+				currentSum = 0;				
+				break;
 			}
-
-			columnIndex++;
 			
-		}
-		rowIndex++;
+			rowIter++;
+			break;			
+		}		
 	}
-	*/
-	return 0;
+	return maxSum;
 }
 
 int main()
 {
+	/*
 	vector<vector<int>> arr
 	{
 		{1,1,1,0,0,0}, 
@@ -136,9 +73,24 @@ int main()
 		{0,0,0,0,0,0},
 		{0,0,0,0,0,0}
 	};
+	*/
+	vector<vector<int>> arr
+	{
+		{-1,1,-1,0,0,0},
+		{0,-1,0,0,0,0},
+		{-1,-1,-1,0,0,0},
+		{0,-9,2,-4,-4,0},
+		{-7,0,0,-2,0,0},
+		{0,0,-1,-2,-4,0}
+	};
+
+	
+		
+		
+		
+		
+		
 	
 	int result = hourglassSum(arr);
-	//cout << result << "\n";
 
-    //std::cout << "Hello World!\n"; 
 }
